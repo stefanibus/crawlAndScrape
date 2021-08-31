59c98e4e130c3371f933fece5fe602ec70b568ba
Stefanibus <stefan.brueck@posteo.de>
2021-08-31 14:15:21 +0200

    added two  npm packages: cherioo and request

--- next.config.js
+++ next.config.js
@@ -11,4 +11,9 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-});
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; 
+    return config;
+  },
+}); 
